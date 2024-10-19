a3993a5db3862df5285040f878ce226ff9601e9a
Nadyawulandar1 <wnadya05@gmail.com>
2024-10-19 16:48:35 -0700

    feat/backend

--- pokedex/backend/generateJsonDB.js
+++ pokedex/backend/generateJsonDB.js
@@ -1,11 +1,28 @@
 const fs = require("fs");
+const axios = require('axios').default
+
+console.log('test');
 
 async function generateJsonDB() {
   // TODO: fetch data pokemon api dan buatlah JSON data sesuai dengan requirement.
   // json file bernama db.json. pastikan ketika kalian menjalankan npm run start
   // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
   // pokemon yang telah kalian parsing dari public api pokemon
-  const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/";
+  const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
+
+  // 1. FETCH API
+  const response = await axios.get(pokemonApiURL)
+  console.log(response.data.results);
+
+  // 2. Write data ke db.json
+  const sample = {
+    "pokemon": []
+  }
+
+  sample.pokemon = response.data.results
+
+  fs.writeFileSync('db.json', JSON.stringify(sample, null, 4))
+
 }
 
 generateJsonDB();
