faa4d5ea860ae4118677e78e9001da03ebddec38
lotusCupcake <fikriansari.mfa@gmail.com>
2024-10-19 18:58:53 +0700

    feat: bulk-insert, get-all and reset-db

--- seeder/index.js
+++ seeder/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const fs = require("fs");
 
 async function main() {
   /**--------------- Not allowed to be edited - start - --------------------- */
@@ -18,14 +19,46 @@ async function main() {
   });
 
   // Define a schema for the collection
-  const schema = new mongoose.Schema({}, { strict: false });
+  const schema = new mongoose.Schema(
+    {
+      title: String,
+      year: Number,
+      genre: [String],
+      description: String,
+      director: String,
+      cast: [String],
+    },
+    { strict: false }
+  );
   const Model = mongoose.model(collection, schema);
 
   switch (command) {
     case "check-db-connection":
       await checkConnection();
       break;
-    // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
+    case "bulk-insert":
+      const data = JSON.parse(fs.readFileSync("seed.json", "utf8"));
+      for (const item of data) {
+        await Model.create({
+          title: item.title,
+          year: item.year,
+          genre: item.genre,
+          description: item.description,
+          director: item.director,
+          cast: item.cast,
+        });
+      }
+      console.log("bulk insert success");
+      break;
+    case "get-all":
+      const result = await Model.find();
+      console.log(result);
+      console.log("get all success");
+      break;
+    case "reset-db":
+      await Model.deleteMany();
+      console.log("reset db success");
+      break;
     default:
       throw Error("command not found");
   }
