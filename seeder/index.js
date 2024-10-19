0d912f90a51a250957b4d4e3a98b1f4887dd358c
RIFKY <rifkiardiansah96@gmail.com>
2024-10-19 13:11:26 +0700

    Fetch : Seeder

--- seeder/index.js
+++ seeder/index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
 require("dotenv").config();
 
 async function main() {
@@ -11,20 +12,63 @@ async function main() {
   const command = args[0];
   /**--------------- Not allowed to be edited - end - --------------------- */
 
-  // Connect to MongoDB
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(mongoUri);
 
-  // Define a schema for the collection
-  const schema = new mongoose.Schema({}, { strict: false });
+  const schema = new mongoose.Schema(
+    {
+      title: String,
+      year: Number,
+      genre: [String],
+      description: String,
+      director: String,
+      cast: [String],
+    },
+    { strict: false }
+  );
+
   const Model = mongoose.model(collection, schema);
 
   switch (command) {
     case "check-db-connection":
       await checkConnection();
       break;
+    case "bulk-insert":
+      const data = JSON.parse(fs.readFileSync("seed.json", "utf-8"));
+      for (const movie of data) {
+        // const movieModel = new Model();
+        // movieModel.title = movie.title;
+        // movieModel.year = movie.year;
+        // movieModel.genre = movie.genre;
+        // movieModel.description = movie.description;
+        // movieModel.director = movie.director;
+        // movieModel.cast = movie.cast;
+        // await movieModel.save();
+
+        await Model.create({
+          title: movie.title,
+          year: movie.year,
+          genre: movie.genre,
+          description: movie.description,
+          director: movie.director,
+          cast: movie.cast,
+        });
+      }
+      console.log("Bulk Insert Berhasil");
+
+      break;
+    case "get-all":
+      const movieDataGetAll = await Model.find();
+
+      console.log(movieDataGetAll);
+
+      console.log("Get Data Berhasil");
+
+      break;
+    case "reset-db":
+      await Model.deleteMany();
+      console.log("Reset Data Berhasil");
+      break;
+
     // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
     default:
       throw Error("command not found");
