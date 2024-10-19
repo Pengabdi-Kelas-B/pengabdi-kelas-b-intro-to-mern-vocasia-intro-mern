88a24e6bc1198a5dbcc33529edda37f014444c91
Dimas Adi Syahputra <adisqd77@gmail.com>
2024-10-19 02:13:47 -0500

    Menambah Seeder

--- seeder/index.js
+++ seeder/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const fs = require("fs");
 
 async function main() {
   /**--------------- Not allowed to be edited - start - --------------------- */
@@ -12,18 +13,54 @@ async function main() {
   /**--------------- Not allowed to be edited - end - --------------------- */
 
   // Connect to MongoDB
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(mongoUri);
 
   // Define a schema for the collection
-  const schema = new mongoose.Schema({}, { strict: false });
+  const schema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    genre: [String],
+    description: String,
+    director: String,
+    cast: [String]
+  }, { strict: false });
+
   const Model = mongoose.model(collection, schema);
 
   switch (command) {
     case "check-db-connection":
       await checkConnection();
+      break;
+    case "bulk-insert":
+      const data = JSON.parse(fs.readFileSync("seed.json", 'utf-8'))
+
+      for (const movie of data) {
+        const movieModel = new Model()
+        movieModel.title = movie.title
+        movieModel.year = movie.year
+        movieModel.genre = movie.genre
+        movieModel.description = movie.description
+        movieModel.director = movie.director
+        movieModel.cast = movie.cast
+
+        await movieModel.save()
+      }
+      console.log("Bulk Insert Berhasil");
+      
+      break;
+      case "get-all":
+      const movieDataGetAll = await Model.find()
+
+      console.log(movieDataGetAll);
+
+      console.log("Get Data Berhasil");
+      
+      break;
+      case "reset-db":
+        await Model.deleteMany()
+
+      console.log("Reset Data Berhasil");
+      
       break;
     // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
     default:
@@ -43,6 +80,9 @@ async function checkConnection() {
     console.error("MongoDB connection failed:", err);
   }
   console.log("check db connection ended...");
+}
+async function bulkInsert() {
+
 }
 
 main();
