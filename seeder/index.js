a8acc78888f235f36ae9d7baf3bc05b605b1fc1e
anisfn <anisafiana62@gmail.com>
2024-10-20 04:38:44 +0700

    feat: seeder

--- seeder/index.js
+++ seeder/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const fs = require("fs");
 
 async function main() {
   /**--------------- Not allowed to be edited - start - --------------------- */
@@ -12,13 +13,17 @@ async function main() {
   /**--------------- Not allowed to be edited - end - --------------------- */
 
   // Connect to MongoDB
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(mongoUri);
 
   // Define a schema for the collection
-  const schema = new mongoose.Schema({}, { strict: false });
+  const schema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    genre: [String],
+    description: String,
+    director: String,
+    cast: [String]
+  }, { strict: false });
   const Model = mongoose.model(collection, schema);
 
   switch (command) {
@@ -26,6 +31,34 @@ async function main() {
       await checkConnection();
       break;
     // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
+    case "bulk-insert":
+      const data = JSON.parse(fs.readFileSync("seed.json", 'utf-8'))
+      for (const movie of data) {
+        await Model.create({
+          title: movie.title,
+          year: movie.year,
+          genre: movie.genre,
+          description: movie.description,
+          director: movie.director,
+          cast: movie.cast
+        })
+      }
+      console.log("Bulk Insert Berhasil!");
+      break;
+
+      case "get-all":
+        const movieDataGetAll = await Model.find();
+        console.log(movieDataGetAll);
+        console.log("Get Data Berhasil!");
+        break;
+        
+        case "reset-db":
+          await Model.deleteMany();
+          console.log("Reset Data Berhasil!");
+          break;
+          
+        
+
     default:
       throw Error("command not found");
   }
