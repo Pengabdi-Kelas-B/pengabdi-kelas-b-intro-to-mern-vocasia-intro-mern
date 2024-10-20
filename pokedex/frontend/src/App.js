6097b93365fe63fb1e8b568334b6ca517125ca03
lotusCupcake <fikriansari.mfa@gmail.com>
2024-10-20 21:28:28 +0700

    feat: flip card

--- pokedex/frontend/src/App.js
+++ pokedex/frontend/src/App.js
@@ -13,12 +13,22 @@ function App() {
   useEffect(() => {
     getPokemons();
   }, []);
+
+  const [flippedId, setFlippedId] = useState(null);
+  const handleFlip = (id) => {
+    setFlippedId(flippedId === id ? null : id);
+  };
   return (
     <div className="font-poppins flex flex-col items-center justify-center min-h-screen py-12 px-2">
       <h1 className="text-4xl font-bold mb-7">{name}</h1>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
         {pokemons.map((pokemon) => (
-          <Thumbnail key={pokemon.id} pokemon={pokemon} />
+          <Thumbnail
+            key={pokemon.id}
+            pokemon={pokemon}
+            isFlipped={flippedId === pokemon.id}
+            onFlip={() => handleFlip(pokemon.id)}
+          />
         ))}
       </div>
     </div>
--- pokedex/frontend/src/components/Thumbnail.js
+++ pokedex/frontend/src/components/Thumbnail.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Thumbnail = ({ pokemon }) => {
+const Thumbnail = ({ pokemon, isFlipped, onFlip }) => {
   const typeColors = {
     rock: "bg-[#fffbeb]",
     ghost: "bg-[#f7f7f7]",
@@ -23,24 +23,47 @@ const Thumbnail = ({ pokemon }) => {
   };
 
   return (
-    <div
-      className={`flex flex-col items-center justify-center p-6 m-1 border border-gray-200 rounded-[10px] w-[200px] text-center shadow-md h-[300px] transition-all duration-200 ease-in-out hover:scale-[1.02] hover:shadow-lg ${
-        typeColors[pokemon.types[0]]
-      }`}
-      key={pokemon.id}
-    >
-      <div className="rounded-xl p-1 bg-white/30 mb-2">
-        <small className="capitalize">#0{pokemon.id}</small>
-      </div>
-      <img
-        className="w-full h-[150px] mb-2 object-contain"
-        src={pokemon.image}
-        alt={pokemon.name}
-      />
-      <div className="flex flex-col w-full">
-        <h3 className="font-bold mb-2 capitalize">{pokemon.name}</h3>
-        <small className="capitalize">{pokemon.types[0]}</small>
-      </div>
+    <div onClick={onFlip}>
+      {!isFlipped ? (
+        <div
+          className={`flex flex-col items-center justify-center p-6 m-1 border border-gray-200 rounded-[10px] w-[200px] text-center shadow-md h-[300px] transition-all duration-200 ease-in-out hover:scale-[1.02] hover:shadow-lg ${
+            typeColors[pokemon.types[0]]
+          }`}
+        >
+          <div className="rounded-xl p-1 bg-white/30 mb-2">
+            <small className="capitalize">#0{pokemon.id}</small>
+          </div>
+          <img
+            className="w-full h-[150px] mb-2 object-contain"
+            src={pokemon.image}
+            alt={pokemon.name}
+          />
+          <div className="flex flex-col w-full">
+            <h3 className="font-bold mb-2 capitalize">{pokemon.name}</h3>
+            <small className="capitalize">{pokemon.types[0]}</small>
+          </div>
+        </div>
+      ) : (
+        <div
+          className={`flex flex-col items-center justify-center p-6 m-1 border border-gray-200 rounded-[10px] w-[200px] shadow-md h-[300px] transition-all duration-200 ease-in-out hover:scale-[1.02] hover:shadow-lg ${`${
+            typeColors[pokemon.types[0]]
+          } bg-opacity-50`}`}
+        >
+          <h3 className="font-bold mb-2">Additional Info</h3>
+          <p>Weight: {pokemon.weight} kg</p>
+          <p>Height: {pokemon.heigh} cm</p>
+          <div className="mt-2">
+            <h4 className="font-semibold mb-1">Abilities:</h4>
+            <ul className="list-disc list-inside">
+              {pokemon.abilities.slice(0, 3).map((ability, index) => (
+                <li key={index} className="capitalize">
+                  {ability}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
