5977f67e81a4f823b197ff526b4f3b56d7225ca7
zulfikri1704 <zul170403@gmail.com>
2024-10-20 14:37:35 +0700

    tambah seeder

--- seeder/index.js
+++ seeder/index.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const fs = require('fs');
+const { title } = require("process");
 require("dotenv").config();
 
 async function main() {
@@ -12,19 +14,54 @@ async function main() {
   /**--------------- Not allowed to be edited - end - --------------------- */
 
   // Connect to MongoDB
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(mongoUri);
 
   // Define a schema for the collection
-  const schema = new mongoose.Schema({}, { strict: false });
+  const schema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    genre: [String],
+    descrption: String,
+    director: String,
+    cast:[String]
+  }, { strict: false });
   const Model = mongoose.model(collection, schema);
 
   switch (command) {
     case "check-db-connection":
       await checkConnection();
       break;
+      case "bulk-insert":
+        const data =JSON.parse (fs.readFileSync("seed.json", 'utf-8'))
+
+        for (const movie of data) {
+          const movieModel = new Model()
+          movieModel.title = movie.title
+          movieModel.year = movie.year
+          movieModel.genre = movie.genre
+          movieModel.descrption = movie.descrption
+          movieModel.director = movie.director
+          movieModel.cast = movie.cast
+
+          await movieModel.save()
+    
+        }
+        console.log("Bulk Inset Berhasil");
+
+        break
+        case "get-all":
+          const movieDataGetAll = await Model.find()
+
+          console.log(movieDataGetAll);
+          console.log("Get Data Berhasil");
+          
+          break
+
+          case "reset-db":
+           await Model.deleteMany()
+        
+            console.log("reset Data Berhasil");
+            break
     // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
     default:
       throw Error("command not found");
@@ -44,5 +81,5 @@ async function checkConnection() {
   }
   console.log("check db connection ended...");
 }
-
+ 
 main();
