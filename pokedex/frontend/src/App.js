2cd12e6c4760ae2a0eabfb72b8aec40a2d09399e
lotusCupcake <fikriansari.mfa@gmail.com>
2024-10-20 15:07:47 +0700

    feat: frontend for view data from backend

---
+++ pokedex/frontend/src/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Thumbnail from "./components/Thumbnail";
+
+function App() {
+  const name = "Pokedex App";
+  const url = "http://localhost:3300/data";
+  const [pokemons, setPokemons] = useState([]);
+  const getPokemons = async () => {
+    const response = await axios.get(url);
+    setPokemons(response.data);
+  };
+  useEffect(() => {
+    getPokemons();
+  }, []);
+  return (
+    <div className="font-poppins flex flex-col items-center justify-center min-h-screen py-12 px-2">
+      <h1 className="text-4xl font-bold mb-7">{name}</h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        {pokemons.map((pokemon) => (
+          <Thumbnail key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ pokedex/frontend/src/components/Thumbnail.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Thumbnail = ({ pokemon }) => {
+  const typeColors = {
+    rock: "bg-[#fffbeb]",
+    ghost: "bg-[#f7f7f7]",
+    electric: "bg-[#ffffa1]",
+    bug: "bg-[#f6d6a7]",
+    poison: "bg-[#ede9fe]",
+    normal: "bg-[#f4f4f4]",
+    fairy: "bg-[#fbcfe8]",
+    fire: "bg-[#fee2e2]",
+    grass: "bg-[#e2f9e1]",
+    water: "bg-[#e0f1fd]",
+    dragon: "bg-[#e0e7ff]",
+    ground: "bg-[#fed7aa]",
+    fighting: "bg-[#e5e5e5]",
+    psychic: "bg-[#FBE3DF]",
+    ice: "bg-[#bae6fd]",
+    flying: "bg-[#E0F1FD]",
+    steel: "bg-[#F4F4F4]",
+    dark: "bg-[#f1f5f9]",
+  };
+
+  return (
+    <div
+      className={`flex flex-col items-center justify-center p-6 m-1 border border-gray-200 rounded-[10px] w-[200px] text-center shadow-md h-[300px] transition-all duration-200 ease-in-out hover:scale-[1.02] hover:shadow-lg ${
+        typeColors[pokemon.types[0]]
+      }`}
+      key={pokemon.id}
+    >
+      <div className="rounded-xl p-1 bg-white/30 mb-2">
+        <small className="capitalize">#0{pokemon.id}</small>
+      </div>
+      <img
+        className="w-full h-[150px] mb-2 object-contain"
+        src={pokemon.image}
+        alt={pokemon.name}
+      />
+      <div className="flex flex-col w-full">
+        <h3 className="font-bold mb-2 capitalize">{pokemon.name}</h3>
+        <small className="capitalize">{pokemon.types[0]}</small>
+      </div>
+    </div>
+  );
+};
+
+export default Thumbnail;
